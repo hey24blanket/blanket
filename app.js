db6bba52f4766fae68354372cb8d7e83a51d8486
hey24blanket <hey24blanket@gmail.com>
2026-09-03 11:01:07 +0900

    Fix dashboard escaping and finalize interactions

--- app.js
+++ app.js
@@ -79,8 +79,8 @@ function uid(prefix = 'id') {
 }
 
 function escapeHtml(value = '') {
-  return String(value).replace(/[&<>'\"]/g, char => ({
-    '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '\"': '&quot;'
+  return String(value).replace(/[&<>'"]/g, char => ({
+    '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;'
   })[char]);
 }
 
